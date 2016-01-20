b1610c9fe97c2930ae5820fdcd8a68a219cc9b16
pedrooliveira23 <pedrooliveira2309@gmail.com>
2016-01-20 16:59:28 -0200

    Organização da página do post

--- content.php
+++ content.php
@@ -10,51 +10,72 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php
-		// Post thumbnail.
-		twentyfifteen_post_thumbnail();
-	?>
+<article
+	id="post-<?php the_ID(); ?>"
+	<?php post_class('container container-post'); ?>>
 
 	<header class="entry-header">
 		<?php
-			if ( is_single() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
-				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-			endif;
+		if ( is_single() ) :
+		the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+		the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		endif;
 		?>
-	</header><!-- .entry-header -->
+	</header>
+	<!-- .entry-header -->
+
+
+	<?php
+	// Post thumbnail.
+	twentyfifteen_post_thumbnail();
+	?>
 
 	<div class="entry-content">
 		<?php
-			/* translators: %s: Name of current post */
-			the_content( sprintf(
+		/* translators: %s: Name of current post */
+		the_content( sprintf(
 				__( 'Continue reading %s', 'twentyfifteen' ),
 				the_title( '<span class="screen-reader-text">', '</span>', false )
-			) );
+		) );
 
-			wp_link_pages( array(
+		wp_link_pages( array(
 				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfifteen' ) . '</span>',
 				'after'       => '</div>',
 				'link_before' => '<span>',
 				'link_after'  => '</span>',
 				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>%',
 				'separator'   => '<span class="screen-reader-text">, </span>',
-			) );
+		) );
 		?>
-	</div><!-- .entry-content -->
+		<aside id="recent_post" class="widget">
+			<?php if ( ! dynamic_sidebar( 'sidebar-primary' ) ) : ?>
+			<h5>Outros</h5>
+			<ul>
+				<?php
+				$recent_posts = wp_get_recent_posts();
+				foreach( $recent_posts as $recent ){
+					echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .get_the_post_thumbnail($recent["ID"]) .'<p class="wid">'.   $recent["post_title"].'</p></a> </li> ';
+				}
+				?>
+			</ul>
+			<?php endif; ?>
+		</aside>
+	</div>
+	<!-- .entry-content -->
 
 	<?php
-		// Author bio.
-		if ( is_single() && get_the_author_meta( 'description' ) ) :
-			get_template_part( 'author-bio' );
-		endif;
+	// Author bio.
+	if ( is_single() && get_the_author_meta( 'description' ) ) :
+	get_template_part( 'author-bio' );
+	endif;
 	?>
 
 	<footer class="entry-footer">
 		<?php twentyfifteen_entry_meta(); ?>
 		<?php edit_post_link( __( 'Edit', 'twentyfifteen' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
+	</footer>
+	<!-- .entry-footer -->
 
-</article><!-- #post-## -->
+</article>
+<!-- #post-## -->
